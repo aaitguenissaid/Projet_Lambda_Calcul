802aabb63a58de668edc32bf8b5d5300246942ef
Kirill <sidorets.kirill97@gmail.com>
2021-03-27 19:57:16 +0100

    1.2.1 done

--- p1_2.v
+++ p1_2.v
@@ -6,7 +6,9 @@ Definition cbool := T -> T -> T.
 
 
 Definition ctr : cbool := fun x y => x.
+Print ctr.
 Definition cfa : cbool := fun x y => y.
+Print cfa.
 
 Definition cif :=
 fun (b : cbool) (t : T) (e : T) =>
@@ -17,18 +19,18 @@ Variables F V : T.
 Compute cif ctr V F.
 Compute cif cfa V F.
 
-Definition cand := fun (a:cbool) (b:cbool) => cif a (cif b V F) F.
+Definition cand := fun (a:cbool) (b:cbool) =>fun (x:T) (y:T) =>a (b x y) y.
 Compute cand ctr ctr.
 Compute cand ctr cfa.
 Compute cand cfa cfa.
 Compute cand cfa ctr.
 
-Definition cor := fun (a:cbool) (b:cbool) => cif a V (cif b V F).
+Definition cor := fun (a:cbool) (b:cbool) =>fun (x:T)(y:T)=>a x (b x y).
 Compute cor ctr ctr.
 Compute cor ctr cfa.
 Compute cor cfa cfa.
 Compute cor cfa ctr.
-Definition cnot := fun (a:cbool) => cif a F V.
+Definition cnot := fun (b:cbool) => fun (x:T) (y:T)=> b y x.
 Compute cnot ctr.
 Compute cnot cfa.
 
